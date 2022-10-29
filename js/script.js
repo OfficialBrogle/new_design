c87d441d82350ca4587d0d52e1f87befc4b4e960
OfficialBrogle <81626018+OfficialBrogle@users.noreply.github.com>
2022-10-29 20:54:22 +0200

    přes js byla přidána funkce active

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
-var img = $('img');
+/*var triple = $('.triple'),
+    style = {
+        position: 'relative'
+    };
 
-console.log(img);
+
+triple.css(style);
+
+triple.on('click', function() {
+    $(this).animate({ left: 1000 }, 2000).fadeOut()
+});
+
+
+var treti = $('.left li').eq(2);
+var posledni = $('.left li:last-child');
+
+$('.left li').addClass('oznaceny');*/
+
+var selected = $('.left li a')
+
+$(selected).on('click', function() {
+    $(selected).removeClass('active');
+    $(this).toggleClass('active');
+    event.preventDefault();
+})
