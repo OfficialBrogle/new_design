35ccdbcdb1d4aec7beb565b90a67592a862c38e4
OfficialBrogle <81626018+OfficialBrogle@users.noreply.github.com>
2022-11-02 15:23:46 +0100

    carousel vložen do vlastního scriptu

---
+++ js/carousel.js
@@ -0,0 +1,54 @@
+var cover = $('#cover'),
+    covers = $('.fadecovers');
+
+covers.children(':not(:last)').hide();
+
+var Slider = {
+    intervalID: null,
+    running: false,
+    config: {
+        fadeSpeed: 1500,
+        delayTime: 3000
+    },
+
+    set: function(id) {
+        this.intervalID = id;
+    },
+
+    get: function() {
+        return this.intervalID;
+    },
+
+    start: function(settings) {
+
+        $.extend(Slider.config, settings);
+
+        this.intervalID = setInterval(function() {
+            covers.children(':last')
+                .fadeOut(Slider.config.fadeSpeed, function() { $(this).prependTo(covers); })
+                .prev().fadeIn(Slider.config.fadeSpeed);
+        }, Slider.config.delayTime);
+
+        this.running = true;
+    },
+
+    pause: function() {
+        clearInterval(this.intervalID);
+        this.intervalID = null;
+
+        this.running = false;
+    },
+
+    resume: function() {
+        if (!this.intervalID) this.start();
+    },
+
+    toggle: function() {
+        if (this.running) this.pause();
+        else this.resume();
+    }
+}
+
+covers.on('click', function() {
+    Slider.toggle();
+});
